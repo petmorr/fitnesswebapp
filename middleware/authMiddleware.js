4da14f539d435b8e4182c6759267a2733f71d1d0
Zuhaib-gif <135388850+Zuhaib-gif@users.noreply.github.com>
2024-04-25 22:22:02 +0100

    Delete middleware directory

--- middleware/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-const authenticateToken = (req, res, next) => {
-  if (req.session.isAuthenticated) {
-    next();
-  } else {
-      res.redirect('/login');
-  }
-};
-
-module.exports = authenticateToken;
